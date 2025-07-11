05d116c2eda26abee43edec459fc6d5e68059c98
satya prakash <satya200prakash@gmail.com>
2025-07-12 00:03:43 +0530

    change multer login(diskstorage to memorstorage)

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,12 +1,13 @@
 import multer from "multer";
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "./public/downloads")
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: function(req, file, cb) {
+//         cb(null, "./public/downloads")
+//     },
+//     filename: function(req, file, cb) {
+//         cb(null, file.originalname)
+//     }
+// })
+const storage = multer.memoryStorage();
 
 export const upload = multer({
     storage,
