4d4ef8d5118ceadc1977952f102b0dfd6901cf7b
satya prakash <satya200prakash@gmail.com>
2025-07-12 00:05:09 +0530

    chaange cloudinary logic-> add streamifier for store buffer

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
+import streamifier from "streamifier";
 import fs from "fs";
 import { ApiError } from "./ApiError.js";
 cloudinary.config({
@@ -6,25 +7,44 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
-const uploadOnCloudinary = async(localFilePath) => {
-    // console.log(localFilePath);
-    
+
+const uploadOnCloudinary = async(buffer) {
     try {
-        if(!localFilePath) return null;
-        const uploadResponse = await cloudinary.uploader.upload(localFilePath,{
-            resource_type: "raw"
-        })
-        // console.log(uploadResponse.url);
-        
-        fs.unlinkSync(localFilePath);
-        // console.log("Public url", uploadResponse.secure_url);
-        
-        return uploadResponse;
+    return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { resource_type: 'raw' },
+      (error, result) => {
+        if (result) resolve(result);
+        else reject(error);
+      }
+    );
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
     } catch (error) {
-        fs.unlinkSync(localFilePath)
-        return null;
+        console.error("Failed upload on cloudinary");
+        
     }
 }
+
+// const uploadOnCloudinary = async(localFilePath) => {
+//     // console.log(localFilePath);
+    
+//     try {
+//         if(!localFilePath) return null;
+//         const uploadResponse = await cloudinary.uploader.upload(localFilePath,{
+//             resource_type: "raw"
+//         })
+//         // console.log(uploadResponse.url);
+        
+//         fs.unlinkSync(localFilePath);
+//         // console.log("Public url", uploadResponse.secure_url);
+        
+//         return uploadResponse;
+//     } catch (error) {
+//         fs.unlinkSync(localFilePath)
+//         return null;
+//     }
+// }
 
 export {
     uploadOnCloudinary
