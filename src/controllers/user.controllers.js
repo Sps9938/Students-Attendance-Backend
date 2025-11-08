58a66a95c599f2ab48aa9c6590b8c23f3d7f6178
satya prakash <satya200prakash@gmail.com>
2025-11-08 18:36:22 +0530

    used Brevo email

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -39,14 +39,15 @@ const sendOtp = asyncHandler(async (req, res) => {
 
 
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS,
-    },
-  })
-
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, 
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
+  },
+});
 
 
   const mailOptions = {
