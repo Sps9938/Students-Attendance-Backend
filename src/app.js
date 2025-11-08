f7705ff26867c2473f439c37143dc2ab866fa9a3
satya prakash <satya200prakash@gmail.com>
2025-11-08 16:35:30 +0530

    update in app.js

--- src/app.js
+++ src/app.js
@@ -70,4 +70,4 @@ app.use((err, req, res, next) => {
 
 //https://localhost:4000/api/v1/teacher/register
 
-export { app };
+module.exports = app;
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -38,13 +38,15 @@ const sendOtp = asyncHandler(async (req, res) => {
   });
 
 
+
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS,
     },
-  });
+  })
+
 
 
   const mailOptions = {
